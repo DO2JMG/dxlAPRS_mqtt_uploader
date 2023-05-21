c1e990ed301f21ce8244d70682effaa4561e79db
DO2JMG <34538443+DO2JMG@users.noreply.github.com>
2023-05-21 15:51:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,9 +21,16 @@ using namespace std;
 struct uploader_config {
   int udpport;
   int mqttport;
+  double latitude;
+  double longitude;
   string mqttaddress;
-  string mqttsubscribe;
+  string mqtt_prefix_sondedata;
+  string mqtt_prefix_beacon;
+  string call;
 };
+
+int sockfd;
+struct sockaddr_in servaddr, cliaddr;
 
 uploader_config config;
 
@@ -38,7 +45,7 @@ private: // MQTT
     virtual void onConnected(int /*rc*/) override {
         std::cout << "Connected to MQTT broker" << std::endl;
 
-        subscribe(config.mqttsubscribe, MQTT::QoS::QoS0);
+        subscribe(config.mqtt_prefix_sondedata, MQTT::QoS::QoS0);
     }
     
     virtual void onMessageReceived(const struct mosquitto_message& msg) override {
@@ -47,11 +54,47 @@ private: // MQTT
     }
 };
 
-int main(int argc, char *argv[]) {
-    int sockfd;
-    char buffer[MAXLINE];
-    struct sockaddr_in servaddr, cliaddr;
+void readandsend() {
+    mqttclient client; 
 
+    while (true) {
+        char buffer[MAXLINE];
+
+        socklen_t len;
+        int n;
+   
+        len = sizeof(cliaddr);  
+   
+        n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
+        buffer[n] = '\0';
+
+        client.subscribe(config.mqtt_prefix_sondedata, MQTT::QoS::QoS0);
+        client.publish(config.mqtt_prefix_sondedata, buffer, MQTT::QoS::QoS0);
+    }
+}
+
+void uploadStationInfos()
+{   
+  while (true) {
+    mqttclient client;  
+    
+    string strOut;
+
+    strOut += "{";
+    strOut += "\"call\": \"" + config.call + "\",";
+    strOut += "\"latitude\": " + to_string(config.latitude) + ",";
+    strOut += "\"longitude\": " + to_string(config.longitude) + "";
+    strOut += "}";
+
+    client.subscribe(config.mqtt_prefix_beacon, MQTT::QoS::QoS0);
+    client.publish(config.mqtt_prefix_beacon, strOut.c_str(), MQTT::QoS::QoS0);
+
+    sleep(60);
+    
+  }
+}
+
+int main(int argc, char *argv[]) {
     for (int i = 0; i<argc; i++) {
         if (strcmp(argv[i],"-h") == 0 || strcmp(argv[i],"-help") == 0) {
             cout << "\n----------------------------------------------------\n";
@@ -61,9 +104,13 @@ int main(int argc, char *argv[]) {
             cout << "Parameter\tDescription\n\n";
             cout << "-h\t\tthis help\n";
             cout << "-p\t\tudp receive port\n";
+            cout << "-lat\t\thome location latitude\n";
+            cout << "-lon\t\thome location longitude\n";
+            cout << "-call\t\tcallsign\n";
             cout << "-ma\t\tmqtt server address\n";
-            cout << "-mp\t\tmqtt server port\n\n";
-            cout << "-mp\t\tmqtt server prefix\n\n";
+            cout << "-mp\t\tmqtt server port\n";
+            cout << "-mb\t\tmqtt server prefix station beacon\n";
+            cout << "-ms\t\tmqtt server prefix sondedata\n\n";
             return 0;
         }
         if (strcmp(argv[i],"-p") == 0) {
@@ -92,9 +139,41 @@ int main(int argc, char *argv[]) {
         } 
         if (strcmp(argv[i],"-ms") == 0) {
             if(i+1 < argc) {
-                config.mqttsubscribe = argv[i+1];
+                config.mqtt_prefix_sondedata = argv[i+1];
             } else {
                 cout << "Error : mqtt prefix\n"; 
+                return 0;
+            }
+        } 
+        if (strcmp(argv[i],"-lat") == 0) {
+            if(i+1 < argc) {
+                config.latitude = stod(argv[i+1]);
+            } else {
+                cout << "Error : latitude\n"; 
+                return 0;
+            }
+        } 
+        if (strcmp(argv[i],"-lon") == 0) {
+            if(i+1 < argc) {
+                config.longitude = stod(argv[i+1]);
+            } else {
+                cout << "Error : longitude\n"; 
+                return 0;
+            }
+        } 
+        if (strcmp(argv[i],"-call") == 0) {
+            if(i+1 < argc) {
+                config.call = argv[i+1];
+            } else {
+                cout << "Error : call\n"; 
+                return 0;
+            }
+        } 
+        if (strcmp(argv[i],"-mb") == 0) {
+            if(i+1 < argc) {
+                config.mqtt_prefix_beacon = argv[i+1];
+            } else {
+                cout << "Error : station info\n"; 
                 return 0;
             }
         } 
@@ -117,19 +196,11 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
     
-    mqttclient client; 
-
-    while (true) {
-        socklen_t len;
-        int n;
    
-        len = sizeof(cliaddr);  
-   
-        n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
-        buffer[n] = '\0';
-      
-        client.publish(config.mqttsubscribe, buffer, MQTT::QoS::QoS0);
-    }
+    thread thread_readwrite (readandsend); 
+    thread thread_beacon (uploadStationInfos); 
+    thread_readwrite.join();
+    thread_beacon.join();
     
     return 0;
 }
